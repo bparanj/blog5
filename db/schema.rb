aa5afb2546fe7a17edd9ca2c46881b69f1262799
Bala Paranj <bparanj@onekingslane.com>
2016-03-09 17:31:23 -0800

    e5 alternative for with_scope in Rails 5

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310004805) do
+ActiveRecord::Schema.define(version: 20160310012725) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.boolean  "complete"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "project_id"
   end
 
 end
