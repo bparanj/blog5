6dff8e64d675bb3e91c6ac19e07b555953f48a93
Bala Paranj <bparanj@onekingslane.com>
2016-03-10 11:58:14 -0800

    updated method with keyword argument

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :project
   
-  def self.find_incomplete(limit)
+  def self.find_incomplete(limit: 1)
     where(complete: false).order('created_at DESC').limit(limit)
   end
 end
