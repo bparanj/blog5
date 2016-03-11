2482a9524ea931038e84d05f00301ccb516a961e
Bala Paranj <bparanj@onekingslane.com>
2016-03-10 18:14:39 -0800

    e9 and e10

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  def full_name
+    name = first_name + ' '
+    name << "#{middle_initial}. " unless middle_initial.nil?
+    name << last_name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310012725) do
+ActiveRecord::Schema.define(version: 20160310234944) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20160310012725) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "project_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "middle_initial"
+    t.string   "last_name"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end
