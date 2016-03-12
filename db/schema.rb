2a267f61063516d1059f73412b5be6d8e96454eb
Bala Paranj <bparanj@onekingslane.com>
2016-03-11 16:32:26 -0800

    added e14

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311191302) do
+ActiveRecord::Schema.define(version: 20160311233916) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160311191302) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "project_id"
+    t.integer  "priority"
   end
 
   create_table "users", force: :cascade do |t|
