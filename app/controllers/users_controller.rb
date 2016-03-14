b7a0ec68786d41857aaa4a1047b92359203bba0f
Bala Paranj <bala.paranj@zepho.com>
2016-03-13 23:08:29 -0700

    edit notes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  layout :user_layout 
+  
   def index
     @users = User.all
   end
@@ -11,6 +13,20 @@ class UsersController < ApplicationController
     User.create(params.require(:user).permit(:full_name, :password))
     
     redirect_to users_path
+  end
+  
+  private
+
+  def user_layout
+    if admin?
+      'admin'
+    else
+      'application'
+    end
+  end
+
+  def admin?
+    true
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,15 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 
-wealthy = Project.create(name: 'Wealth Building')
-wealthy.tasks.create(name: 'Get rich quick', complete: false, priority: 4)
-wealthy.tasks.create(name: 'Write a book', complete: true, priority: 5)
+# wealthy = Project.create(name: 'Wealth Building')
+# wealthy.tasks.create(name: 'Get rich quick', complete: false, priority: 4)
+# wealthy.tasks.create(name: 'Write a book', complete: true, priority: 5)
+#
+# happy = Project.create(name: 'Be Happy')
+# happy.tasks.create(name: 'Buy a puppy', complete: false, priority: 9)
+# happy.tasks.create(name: 'Dance in the rain', complete: true, priority: 7)
 
-happy = Project.create(name: 'Be Happy')
-happy.tasks.create(name: 'Buy a puppy', complete: false, priority: 9)
-happy.tasks.create(name: 'Dance in the rain', complete: true, priority: 7)
-
-# User.create(first_name: 'Bugs', last_name: 'Bunny')
-# User.create(first_name: 'Daffy', last_name: 'Duck')
-# User.create(first_name: 'Donald', middle_initial: 'Jack Ass', last_name: 'Trump')
-# User.create(first_name: 'David', middle_initial: 'Avacodo', last_name: 'Wolfe')
+User.create(first_name: 'Bugs', last_name: 'Bunny')
+User.create(first_name: 'Daffy', last_name: 'Duck')
+User.create(first_name: 'Donald', middle_initial: 'Jack Ass', last_name: 'Trump')
+User.create(first_name: 'David', middle_initial: 'Avacodo', last_name: 'Wolfe')
