415357b4b1a234b54db4ccf475f58d67ef675d08
Bala Paranj <bparanj@onekingslane.com>
2016-03-14 15:35:04 -0700

    e17

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311233916) do
+ActiveRecord::Schema.define(version: 20160314202712) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_products", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price",      precision: 8, scale: 2
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,20 @@
 # happy.tasks.create(name: 'Buy a puppy', complete: false, priority: 9)
 # happy.tasks.create(name: 'Dance in the rain', complete: true, priority: 7)
 
-User.create(first_name: 'Bugs', last_name: 'Bunny')
-User.create(first_name: 'Daffy', last_name: 'Duck')
-User.create(first_name: 'Donald', middle_initial: 'Jack Ass', last_name: 'Trump')
-User.create(first_name: 'David', middle_initial: 'Avacodo', last_name: 'Wolfe')
+# User.create(first_name: 'Bugs', last_name: 'Bunny')
+# User.create(first_name: 'Daffy', last_name: 'Duck')
+# User.create(first_name: 'Donald', middle_initial: 'Jack Ass', last_name: 'Trump')
+# User.create(first_name: 'David', middle_initial: 'Avacodo', last_name: 'Wolfe')
+
+p = Product.new(name: 'rug', price: 100)
+Product.create(name: 'bowl', price: 20.95)
+Product.create(name: 'pillow', price: 90)
+Product.create(name: 'light', price: 10.95)
+
+c = Category.create(name: 'Clothes')
+Category.create(name: 'Furniture')
+Category.create(name: 'Groceries')
+Category.create(name: 'Electronics')
+
+p.categories << c
+p.save!
