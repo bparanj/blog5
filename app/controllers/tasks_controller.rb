05d15d171f3fcbdf98c589ab8e1c42ba858f9994
Bala Paranj <bparanj@onekingslane.com>
2016-03-18 15:37:45 -0700

    e33

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,8 +44,7 @@ class TasksController < ApplicationController
   # PATCH/PUT /tasks/1.json
   def update
     respond_to do |format|
-      logger.info @task.valid?
-      if @task.valid? and @task.update(task_params)
+      if @task.update(task_params)
         format.html { redirect_to @task, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :project, counter_cache: true
   has_many :comments
 
-  validates_with DatetimeValidator
+  validates_datetime :due_date
   
   def self.find_incomplete(limit: 1)
     where(complete: false).order('created_at DESC').limit(limit)
