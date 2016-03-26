f46fb05d0a2444299ee449a33eed000ac7997761
Bala Paranj <bparanj@onekingslane.com>
2016-03-26 10:38:10 -0700

    e40

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
       page_title 
     end 
   end
+  
+  def admin_area(&block)
+    # content_tag(:div, block.call, class: 'admin')
+    
+    concat('<div class="admin">', block.binding)
+    yield
+    concat('</div>', block.binding)
+  end
 end
